a05d8904734302f67e04b5786dfa6ed1f8e7f8ef
suyalneeraj19 <148360932+suyalneeraj19@users.noreply.github.com>
2023-12-02 19:58:51 +0530

    Update index.js
    making it touch responsive

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Game Constants & Variables
-let inputDir = {x: 0, y: 0}; 
+let inputDir = { x: 0, y: 0 };
 const foodSound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameover.mp3');
 const moveSound = new Audio('music/move.mp3');
@@ -8,16 +8,16 @@ let speed = 19;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
-    {x: 13, y: 15}
+    { x: 13, y: 15 }
 ];
 
-food = {x: 6, y: 7};
+let food = { x: 6, y: 7 };
 
 // Game Functions
 function main(ctime) {
     window.requestAnimationFrame(main);
-   
-    if((ctime - lastPaintTime)/1000 < 1/speed){
+
+    if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
         return;
     }
     lastPaintTime = ctime;
@@ -25,51 +25,46 @@ function main(ctime) {
 }
 
 function isCollide(snake) {
-    // If you bump into yourself 
+    // If you bump into yourself
     for (let i = 1; i < snakeArr.length; i++) {
-        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
+        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
             return true;
         }
     }
     // If you bump into the wall
-    if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
+    if (snake[0].x >= 18 || snake[0].x <= 0 || snake[0].y >= 18 || snake[0].y <= 0) {
         return true;
     }
-        
+
     return false;
 }
 
-function gameEngine(){
+function gameEngine() {
     // Part 1: Updating the snake array & Food
-    if(isCollide(snakeArr)){
+    if (isCollide(snakeArr)) {
         gameOverSound.play();
         musicSound.pause();
-        inputDir =  {x: 0, y: 0}; 
+        inputDir = { x: 0, y: 0 };
         alert("Game Over. Press any key to play again!");
-        snakeArr = [{x: 13, y: 15}];
+        snakeArr = [{ x: 13, y: 15 }];
         musicSound.play();
-        score = 0; 
+        score = 0;
     }
 
     // If you have eaten the food, increment the score and regenerate the food
-    if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
+    if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
         foodSound.play();
         score += 1;
-        if(score>hiscoreval){
-            hiscoreval = score;
-            localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
-            hiscoreBox.innerHTML = "HiScore: " + hiscoreval;
-        }
         scoreBox.innerHTML = "Score: " + score;
-        snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y});
+        snakeArr.unshift({ x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y });
         let a = 2;
         let b = 16;
-        food = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
+        food = { x: Math.round(a + (b - a) * Math.random()), y: Math.round(a + (b - a) * Math.random()) }
     }
 
     // Moving the snake
-    for (let i = snakeArr.length - 2; i>=0; i--) { 
-        snakeArr[i+1] = {...snakeArr[i]};
+    for (let i = snakeArr.length - 2; i >= 0; i--) {
+        snakeArr[i + 1] = { ...snakeArr[i] };
     }
 
     snakeArr[0].x += inputDir.x;
@@ -78,15 +73,15 @@ function gameEngine(){
     // Part 2: Display the snake and Food
     // Display the snake
     board.innerHTML = "";
-    snakeArr.forEach((e, index)=>{
+    snakeArr.forEach((e, index) => {
         snakeElement = document.createElement('div');
         snakeElement.style.gridRowStart = e.y;
         snakeElement.style.gridColumnStart = e.x;
 
-        if(index === 0){
+        if (index === 0) {
             snakeElement.classList.add('head');
         }
-        else{
+        else {
             snakeElement.classList.add('snake');
         }
         board.appendChild(snakeElement);
@@ -97,53 +92,96 @@ function gameEngine(){
     foodElement.style.gridColumnStart = food.x;
     foodElement.classList.add('food')
     board.appendChild(foodElement);
-
-
 }
-
 
 // Main logic starts here
 musicSound.play();
 let hiscore = localStorage.getItem("hiscore");
-if(hiscore === null){
+if (hiscore === null) {
     hiscoreval = 0;
     localStorage.setItem("hiscore", JSON.stringify(hiscoreval))
 }
-else{
+else {
     hiscoreval = JSON.parse(hiscore);
     hiscoreBox.innerHTML = "HiScore: " + hiscore;
 }
 
 window.requestAnimationFrame(main);
-window.addEventListener('keydown', e =>{
-    inputDir = {x: 0, y: 1} // Start the game
+
+// Event listeners for keyboard input
+window.addEventListener('keydown', e => {
+    inputDir = { x: 0, y: 1 } // Start the game
     moveSound.play();
     switch (e.key) {
         case "ArrowUp":
-            console.log("ArrowUp");
             inputDir.x = 0;
             inputDir.y = -1;
             break;
-
         case "ArrowDown":
-            console.log("ArrowDown");
             inputDir.x = 0;
             inputDir.y = 1;
             break;
-
         case "ArrowLeft":
-            console.log("ArrowLeft");
             inputDir.x = -1;
             inputDir.y = 0;
             break;
-
         case "ArrowRight":
-            console.log("ArrowRight");
             inputDir.x = 1;
             inputDir.y = 0;
             break;
         default:
             break;
     }
-
 });
+
+// Event listeners for touch input
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+document.addEventListener('touchend', handleTouchEnd, false);
+
+let xDown = null;
+let yDown = null;
+
+function handleTouchStart(event) {
+    const firstTouch = event.touches[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(event) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    let xUp = event.touches[0].clientX;
+    let yUp = event.touches[0].clientY;
+
+    let xDiff = xDown - xUp;
+    let yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) {
+            // Swipe left
+            inputDir = { x: -1, y: 0 };
+        } else {
+            // Swipe right
+            inputDir = { x: 1, y: 0 };
+        }
+    } else {
+        if (yDiff > 0) {
+            // Swipe up
+            inputDir = { x: 0, y: -1 };
+        } else {
+            // Swipe down
+            inputDir = { x: 0, y: 1 };
+        }
+    }
+
+    xDown = null;
+    yDown = null;
+}
+
+function handleTouchEnd(event) {
+    xDown = null;
+    yDown = null;
+}
